907124c50fc19f70ca2beea03957fddcb812b2ef
mxngoz <1071998@hr.nl>
2023-11-29 13:59:11 +0100

    rotation erin

--- Challenge.py
+++ Challenge.py
@@ -2,7 +2,7 @@ import pygame
 import os
 import time
 import math
-from Challengefuncties import scale_img
+from Challengefuncties import scale_img, blit_rotate_center
 
 GRASS = scale_img(pygame.image.load(os.path.join('imgs', 'grass.jpg')), 3)
 
@@ -25,38 +25,59 @@ clock = pygame.time.Clock()
 
 
 class MainCar:
+    IMG = RED_CAR
+
     def __init__(self, max_vel, rot_vel) -> None:
         self.max_vel = max_vel
         self.vel = 0
         self.rot_vel = rot_vel
         self.angle = 0
+        self.x, self.y = self.START_POS
 
     def rotate(self, left=False, right=False):
-        if left:
+        if left and self.vel > :
             self.angle -= self.rot_vel
 
         elif right:
             self.angle += self.rot_vel
 
+    def draw(self, screen):
+        blit_rotate_center(screen, self.IMG, (self.x, self.y), self.angle)
 
-def draw(screen, images):
+
+class PlayerCar(MainCar):
+    IMG = RED_CAR
+    START_POS = (30, 350)
+
+
+def draw(screen, images, player_car):
     for img, pos in images:
         screen.blit(img, pos)
 
+    player_car.draw(screen)
+    pygame.display.update()
 
-images = [(GRASS, (0, 0)), (RACE_TRACK, (0, 0)), (RED_CAR, (30, 100))]
+
+images = [(GRASS, (0, 0)), (RACE_TRACK, (0, 0))]
+player_car = PlayerCar(4, 4)
 
 playing = True
 while playing:
     clock.tick(FPS)
 
-    draw(SCREEN, images)
-
-    pygame.display.update()
+    draw(SCREEN, images, player_car)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             playing = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_a]:
+        player_car.rotate(left=True)
+
+    if keys[pygame.K_d]:
+        player_car.rotate(right=True)
 
 
 pygame.quit()
--- Challengefuncties.py
+++ Challengefuncties.py
@@ -10,4 +10,5 @@ def scale_img(img, factor):
 def blit_rotate_center(screen, image, top_left, angle):
     rotated_image = pygame.transform.rotate(image, angle)
     new_rect = rotated_image.get_rect(
-        center=image.get_rect(topLeft=top_left).center)
+        center=image.get_rect(topleft=top_left).center)
+    screen.blit(rotated_image, new_rect.topleft)
