f016f4d0f6c1cc9d5e8f67285587f10218bfbbde
mxngoz <1071998@hr.nl>
2023-11-30 00:09:58 +0100

    Toevoegingen
    Auto kleiner gemaakt, Collision toegevoegd en rem toegevoegd.

--- Challenge.py
+++ Challenge.py
@@ -15,7 +15,7 @@ RACE_TRACK_BORDER_MASK = pygame.mask.from_surface(RACE_TRACK_BORDER)
 FINISH_LINE = pygame.image.load(os.path.join('imgs', 'finish.png'))
 
 RED_CAR = scale_img(pygame.image.load(
-    os.path.join('imgs', 'red-car.png')), 0.4)
+    os.path.join('imgs', 'red-car.png')), 0.3)
 GREEN_CAR = pygame.image.load(os.path.join('imgs', 'green-car.png'))
 
 WIDTH, HEIGHT = RACE_TRACK.get_width(), RACE_TRACK.get_height()
@@ -27,14 +27,16 @@ clock = pygame.time.Clock()
 
 class MainCar:
     IMG = RED_CAR
+    # standaard auto kracht van 2
 
     def __init__(self, max_vel, rot_vel) -> None:
+        self.distance_travelled = 0
         self.max_vel = max_vel
         self.vel = 0
         self.rot_vel = rot_vel
         self.angle = 0
         self.x, self.y = self.START_POS
-        self.accel = 0.3
+        self.accel = 0.02
 
     def rotate(self, left=False, right=False):
         if left and self.vel < self.max_vel\
@@ -51,11 +53,21 @@ class MainCar:
         if right and self.vel == self.max_vel:
             self.angle -= (self.rot_vel / 1.3)
 
+        if left and self.vel < 0:
+            self.angle += self.rot_vel / 1.2
+
+        if right and self.vel < 0:
+            self.angle -= self.rot_vel / 1.2
+
     def draw(self, screen):
         blit_rotate_center(screen, self.IMG, (self.x, self.y), self.angle)
 
     def move_forward(self):
         self.vel = min(self.vel + self.accel, self.max_vel)
+        self.move()
+
+    def move_backwards(self):
+        self.vel = max(self.vel - (self.accel + 0.4), -self.max_vel / 2)
         self.move()
 
     def move(self):
@@ -66,9 +78,12 @@ class MainCar:
         self.x -= horizontal
         self.y -= vertical
 
-    def drag(self, force):
-        self.vel = max(self.vel - force, 0)
+    def drag(self):
+        self.vel = max(self.vel - self.accel, 0)
         self.move()
+
+    def brake(self):
+        self.vel = max(self.vel - self.accel, 0)
 
     def collide(self, mask, x=0, y=0):
         car_mask = pygame.mask.from_surface(self.IMG)
@@ -81,6 +96,17 @@ class MainCar:
 class PlayerCar(MainCar):
     IMG = RED_CAR
     START_POS = (30, 350)
+
+    def bounce(self):
+        if self.vel >= 0 and self.vel <= self.max_vel:
+            self.vel = -1
+            self.move()
+            return
+
+        if self.vel < 0:
+            self.vel = -self.vel
+            self.move()
+            return
 
 
 def draw(screen, images, player_car):
@@ -107,14 +133,19 @@ def move_player(player_car):
         player_car.move_forward()
 
     if not moved:
-        player_car.drag(force=0.03)
+        player_car.drag()
 
     if keys[pygame.K_s]:
-        player_car.drag(force=0.09)
+        moved = True
+        player_car.brake()
+
+        if player_car.vel == 0:
+            player_car.move_backwards()
 
 
 images = [(GRASS, (0, 0)), (RACE_TRACK, (0, 0))]
-player_car = PlayerCar(4, 4)
+player_car = PlayerCar(2.5, 3)
+
 
 playing = True
 while playing:
@@ -128,6 +159,6 @@ while playing:
 
     move_player(player_car)
     if player_car.collide(RACE_TRACK_BORDER_MASK) != None:
-        print('collide')
+        player_car.bounce()
 
 pygame.quit()
